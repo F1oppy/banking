c62de2800764d331347c011962089d472d099f4e
F1oppy <dadheech.aditya041@gmail.com>
2022-06-18 17:13:38 +0530

    Payee Validators

--- src/app/add-payee/add-payee.component.ts
+++ src/app/add-payee/add-payee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-payee',
@@ -14,15 +14,35 @@ export class AddPayeeComponent implements OnInit {
   }
 
   addPayee= new FormGroup({
-    name: new FormControl(""),
-    accno: new FormControl(""),
-    raccno: new FormControl(""),
+    name: new FormControl("",[Validators.required, Validators.minLength(2), Validators.pattern("[a-zA-Z].*")]),
+    accno: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(10)]),
+    raccno: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(10)]),
     saveBen: new FormControl(""),
     nickname: new FormControl(""),
   });
 
   beneficiarySubmitted(){
-    console.log(this.addPayee.value);
+    console.log(this.addPayee.get("name"));
+    console.log(this.addPayee.get("accno"));
+    console.log(this.addPayee.get("raccno"));
+    console.log(this.addPayee.get("saveBen"));
+    console.log(this.addPayee.get("nickname"));
+  }
+
+  get name(): FormControl{
+    return this.addPayee.get("name") as FormControl;
+  }
+  get accno(): FormControl{
+    return this.addPayee.get("accno") as FormControl;
+  }
+  get raccno(): FormControl{
+    return this.addPayee.get("raccno") as FormControl;
+  }
+  get saveBen(): FormControl{
+    return this.addPayee.get("saveBen") as FormControl;
+  }
+  get nickname(): FormControl{
+    return this.addPayee.get("nickname") as FormControl;
   }
 
 }
