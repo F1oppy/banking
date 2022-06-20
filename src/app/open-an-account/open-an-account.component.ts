9761c5683003a2ebc19cbe55df1ebf1ef21c1c75
F1oppy <dadheech.aditya041@gmail.com>
2022-06-21 00:32:30 +0530

    Open An Account Halfway done

--- src/app/open-an-account/open-an-account.component.ts
+++ src/app/open-an-account/open-an-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-open-an-account',
@@ -14,28 +14,86 @@ export class OpenAnAccountComponent implements OnInit {
   }
 
   openAnAccount = new FormGroup({
-    firstName: new FormControl(""),
-    lastName: new FormControl(""),
-    fatherName: new FormControl(""),
-    mobileNumber: new FormControl(""),
-    emailId: new FormControl(""),
-    aadharNumber: new FormControl(""),
-    dOB: new FormControl(""),
-    occupationType: new FormControl(""),
-    sourceOfIncome: new FormControl(""),
-    grossAnnualIncome: new FormControl(""),
-    addressLine1: new FormControl(""),
+    firstName: new FormControl("",[Validators.required]),
+    lastName: new FormControl("",[Validators.required]),
+    fatherName: new FormControl("",[Validators.required]),
+    mobileNumber: new FormControl("",[Validators.required]),
+    emailId: new FormControl("",[Validators.required]),
+    aadharNumber: new FormControl("",[Validators.required]),
+    dOB: new FormControl("",[Validators.required]),
+    occupationType: new FormControl("",[Validators.required]),
+    sourceOfIncome: new FormControl("",[Validators.required]),
+    grossAnnualIncome: new FormControl("",[Validators.required]),
+    addressLine1: new FormControl("",[Validators.required]),
     addressLine2: new FormControl(""),
-    city: new FormControl(""),
-    postalCode: new FormControl(""),
-    country: new FormControl(""),
+    city: new FormControl("",[Validators.required]),
+    postalCode: new FormControl("",[Validators.required]),
+    country: new FormControl("",[Validators.required]),
     landmark: new FormControl(""),
     debitCard: new FormControl(""),
     netBanking: new FormControl(""),
-    termsConditions: new FormControl(""),
+    termsConditions: new FormControl("",[Validators.required]),
   });
 
   openAnAccountSubmitted(){
-    console.log(this.openAnAccount);
+    console.log(this.openAnAccount.get("firstName"));
+  }
+
+  get firstName(): FormControl{
+    return this.openAnAccount.get("firstName") as FormControl;
+  }
+  get lastName(): FormControl{
+    return this.openAnAccount.get("lastName") as FormControl;
+  }
+  get fatherName(): FormControl{
+    return this.openAnAccount.get("fatherName") as FormControl;
+  }
+  get mobileNumber(): FormControl{
+    return this.openAnAccount.get("mobileNumber") as FormControl;
+  }
+  get emailId(): FormControl{
+    return this.openAnAccount.get("emailId") as FormControl;
+  }
+  get aadharNumber(): FormControl{
+    return this.openAnAccount.get("aadharNumber") as FormControl;
+  }
+  get dOB(): FormControl{
+    return this.openAnAccount.get("dOB") as FormControl;
+  }
+  get occupationType(): FormControl{
+    return this.openAnAccount.get("occupationType") as FormControl;
+  }
+  get sourceOfIncome(): FormControl{
+    return this.openAnAccount.get("sourceOfIncome") as FormControl;
+  }
+  get grossAnnualIncome(): FormControl{
+    return this.openAnAccount.get("grossAnnualIncome") as FormControl;
+  }
+  get addressLine1(): FormControl{
+    return this.openAnAccount.get("addressLine1") as FormControl;
+  }
+  get addressLine2(): FormControl{
+    return this.openAnAccount.get("addressLine2") as FormControl;
+  }
+  get city(): FormControl{
+    return this.openAnAccount.get("city") as FormControl;
+  }
+  get postalCode(): FormControl{
+    return this.openAnAccount.get("postalCode") as FormControl;
+  }
+  get country(): FormControl{
+    return this.openAnAccount.get("country") as FormControl;
+  }
+  get landmark(): FormControl{
+    return this.openAnAccount.get("landmark") as FormControl;
+  }
+  get debitCard(): FormControl{
+    return this.openAnAccount.get("debitCard") as FormControl;
+  }
+  get netBanking(): FormControl{
+    return this.openAnAccount.get("netBanking") as FormControl;
+  }
+  get termsConditions(): FormControl{
+    return this.openAnAccount.get("termsConditions") as FormControl;
   }
 }
