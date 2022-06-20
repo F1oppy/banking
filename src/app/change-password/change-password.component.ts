cc0e1fe5a9db0e66aa6d9d57c158803ae71d1dee
pavanreddy71000 <pavanreddy71000@gmail.com>
2022-06-21 00:54:10 +0530

    Login page Validators updated

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -8,18 +8,37 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ChangePasswordComponent implements OnInit {
 
+  changePass: string = 'none';
+  changePin: string = 'none';
   constructor() { }
 
   ngOnInit(): void {
   }
-  changePasswordComponent= new FormGroup({
-    PasswordComponent: new FormControl(""),
-    confirmloginpassword: new FormControl(""),
-    Transactionpassword: new FormControl(""),
-    ConfirmTransactionpassword: new FormControl(""),
+  changepasswordForm= new FormGroup({
+    newpassword: new FormControl("",[Validators.required, Validators.minLength(8), Validators.maxLength(15)]),
+    confirmnewpassword: new FormControl(""),
+    newtransactionpin: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(6), Validators.maxLength(6)]),
+    confirmnewtransactionpin: new FormControl(""),
   });
-  changePasswordSubmitted(){
-    console.log(this.changePasswordComponent.value);
+  changepasswordSubmitted(){
+    if((this.newpassword.value == this.confirmnewpassword.value) && (this.newtransactionpin.value == this.confirmnewtransactionpin.value)){
+      console.log(this.changepasswordForm.value);
+    }
+    else{
+      this.changePass='inline'
+    }
 
+  }
+  get newpassword(): FormControl{
+    return this.changepasswordForm.get("newpassword") as FormControl;
+  }
+  get confirmnewpassword(): FormControl{
+    return this.changepasswordForm.get("confirmnewpassword") as FormControl;
+  }
+  get newtransactionpin(): FormControl{
+    return this.changepasswordForm.get("newtransactionpin") as FormControl;
+  }
+  get confirmnewtransactionpin(): FormControl{
+    return this.changepasswordForm.get("confirmnewtransactionpin") as FormControl;
   }
 }
