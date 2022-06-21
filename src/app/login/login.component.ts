106d17e6d196030c55a3e3dc0919cc052ff37cb3
F1oppy <dadheech.aditya041@gmail.com>
2022-06-21 11:06:53 +0530

    Routing and Reactive form Completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,4 +13,26 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  Login= new FormGroup({
+    userid: new FormControl("",[Validators.required, Validators.minLength(2), Validators.pattern("[a-zA-Z].*")]),
+    newpassword: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(8), Validators.maxLength(10)]),
+    beneficiaryAccNoRe: new FormControl(""),
+    saveBen: new FormControl(""),
+    nickname: new FormControl(""),
+  });
+
+  loginSubmitted(){
+    // if(this.Login.value == this.beneficiaryAccNoRe.value){
+      console.log(this.Login.valid);
+    //   this.repeatAccount = 'none'
+    // }else{
+    //   this.repeatAccount='inline'
+    // }
+  }
+  get userid(): FormControl{
+    return this.Login.get("userid") as FormControl;
+  }
+  get newpassword(): FormControl{
+    return this.Login.get("newpassword") as FormControl;
+  }
 }
--- src/app/register-for-internet-banking/register-for-internet-banking.component.ts
+++ src/app/register-for-internet-banking/register-for-internet-banking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-for-internet-banking',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterForInternetBankingComponent implements OnInit {
 
+  changePass: string = 'none';
+  changePin: string = 'none';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  registerForInternetBanking= new FormGroup({
+    accno: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(10), Validators.maxLength(10)]),
+    newpassword: new FormControl("",[Validators.required, Validators.minLength(8), Validators.maxLength(15)]),
+    confirmpassword: new FormControl("",[Validators.required, Validators.minLength(8), Validators.maxLength(15)]),
+    newtransactionpin: new FormControl("",[Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(6), Validators.maxLength(6)]),
+    confirmnewtransactionpin: new FormControl(""),
+  });
+
+
+  registerSubmitted(){
+    if((this.newpassword.value == this.confirmpassword.value) && (this.newtransactionpin.value == this.confirmnewtransactionpin.value)){
+      console.log(this.registerForInternetBanking.value);
+    }
+    else{
+      this.changePass='inline'
+    }
+    //   this.repeatAccount = 'none'
+    // }else{
+    //   this.repeatAccount='inline'
+    }
+
+
+
+    get accno(): FormControl{
+      return this.registerForInternetBanking.get("accno") as FormControl;
+    }
+    get newpassword(): FormControl{
+      return this.registerForInternetBanking.get("newpassword") as FormControl;
+    }
+    get confirmpassword(): FormControl{
+      return this.registerForInternetBanking.get("confirmpassword") as FormControl;
+    }
+    get newtransactionpin(): FormControl{
+      return this.registerForInternetBanking.get("newtransactionpin") as FormControl;
+    }
+    get confirmnewtransactionpin(): FormControl{
+      return this.registerForInternetBanking.get("confirmnewtransactionpin") as FormControl;
+    }
+  }
+  
+
+
